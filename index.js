6d730a76c4895cf488c9bf6a59294208e7e8ddbc
Jason Kwak <ralph9696@naver.com>
2022-08-27 13:57:52 -0400

    commit tree e6eef5451c30ffe0d9c00efb215b383f8e62feb8

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+function submitData(firstName, email) {
+    const userInfo = {
+        "name": firstName,
+        "email": email
+    };
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(userInfo)
+    };
+    return fetch('http://localhost:3000/users', configurationObject)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (body) {
+            return addNewContact(body.id)
+        })
+        .catch((error) => {
+            document.body.innerHTML = error.message
+        })
+}
+
+function addNewContact(newUser) {
+    document.getElementsByTagName('body')[0].innerHTML = newUser
+}
+
+submitData('name', 'name@name.com')
